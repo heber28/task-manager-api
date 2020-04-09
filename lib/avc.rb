be90cedf09c5e3a447b0eea05dd41ddaf13b44d5
Heber Nunomura <heber28@gmail.com>
2020-04-09 00:05:07 +0000

    adicionado rotas com o constraint de versao

---
+++ lib/avc.rb
@@ -0,0 +1,11 @@
+class Avc
+  def initialize(options)
+    @version = options[:version]
+    @default = options[:default]
+  end
+
+  def matches?(req)
+    @default || req.headers['Accept'].include?("application/vnd.taskmanager.v#(@version)")
+  end
+
+end
